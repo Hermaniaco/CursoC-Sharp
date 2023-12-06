868ea7ab4de8720641b6c8f38236f275ea9acf83
Hermaniaco <43897364+Hermaniaco@users.noreply.github.com>
2023-12-06 19:15:59 -0300

    Add files via upload

---
+++ Readonly.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    public class Cliente
+    {
+        public string Nome;
+
+        // No momento do construtor podemos setar informações, mas depois de lido ele não poderá mais ser mudado
+        readonly DateTime Nascimento;
+
+        public Cliente(string nome, DateTime nascimento)
+        {
+            Nome = nome;
+            Nascimento = nascimento;
+        }
+
+        public string GetDataDeNascimento ()
+        {
+            return String.Format("{0}/{1}/{2}", Nascimento.Day, Nascimento.Month, Nascimento.Year);
+        }
+    }
+
+
+    class Readonly
+    {
+
+        public static void Executar()
+        {
+            var novoCliente = new Cliente("Ana Paula", new DateTime(2020,10, 10));
+
+            Console.WriteLine(novoCliente.Nome);
+            Console.WriteLine(novoCliente.GetDataDeNascimento);
+        }
+    }
+    
+}
