deb30509546cccec5b2b909fc454215e09cf5ee3
Hermaniaco <43897364+Hermaniaco@users.noreply.github.com>
2023-12-16 21:23:08 -0300

    Add files via upload

---
+++ ArrayList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Coleções
+{
+    internal class ArrayList
+    {
+        public static void Executar()
+        {
+            var arraylist = new ArrayList
+            {
+                "Palavra",
+                3,
+                true
+            };
+
+            foreach(var item in arraylist)
+            {
+                Console.WriteLine("{0} => {1}", item, item.GetType());
+            }
+        }
+    }
+}
